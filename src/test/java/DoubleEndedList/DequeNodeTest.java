0f6c646ca2eb663b91e57e98194a910690b2e5fa
cristobalmartinarenas <crimarare@uma.es>
2022-03-15 11:33:29 +0100

    Added some tests to DequeNode

--- src/test/java/DoubleEndedList/DequeNodeTest.java
+++ src/test/java/DoubleEndedList/DequeNodeTest.java
@@ -1,4 +1,56 @@
 package DoubleEndedList;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DequeNodeTest {
+
+    private DequeNode node;
+    private DequeNode node2;
+    private DequeNode node3;
+
+    @BeforeEach
+    public void init(){
+        node = new DequeNode<Integer>(1, null, null);
+    }
+
+    @AfterEach
+    public void finish(){
+        node = null;
+    }
+
+    @Test
+    public void ShouldGetCorrectElementAfterInitialized(){
+        int expectedValue = 1;
+        int obtainedValue = (int) node.getItem();
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void ShouldNotHavePreviousAfterInitialized(){
+        assertNull(node.getPrevious());
+    }
+
+    @Test
+    public void ShouldNotHaveNextAfterInitialized(){
+        assertNull(node.getNext());
+    }
+
+    @Test
+    public void ShouldBeFirstNodeAfterInitialized(){
+        assertTrue(() -> node.isFirstNode());
+    }
+
+    @Test
+    public void ShouldBeLastNodeAfterInitialized(){
+        assertTrue(() -> node.isLastNode());
+    }
+
+    @Test
+    public void ShouldBeTerminalNodeAfterInitialized(){
+        assertFalse(() -> node.isNotATerminalNode());
+    }
 }
