8ba8adb5faebb3e9975f1397997b10f7c4497943
sergioM <sermorepe@gmail.com>
2022-03-22 11:02:41 +0100

    Test Append

--- src/test/java/DoubleEndedList/DoubleLinkedListQueueTest.java
+++ src/test/java/DoubleEndedList/DoubleLinkedListQueueTest.java
@@ -1,8 +1,22 @@
 package DoubleEndedList;
 
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DoubleLinkedListQueueTest {
 
+    @Test
+    public void ShouldAppendANodeLastPosition() {
+        DoubleLinkedListQueue list1 = new DoubleLinkedListQueue<Integer>();
+        DequeNode node1 = new DequeNode<Integer>(1, null, null);
+        DequeNode node2 = new DequeNode<Integer>(1, null, null);
+        list1.append(node1);
+        list1.append(node2);
 
+        assertEquals(list1.peekLast(), node2);
+
+    }
 
 
 }
