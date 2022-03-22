3950b51f578e23fff25ae0c9ef3ec0ed9a51b6e6
sergioM <sermorepe@gmail.com>
2022-03-22 11:47:31 +0100

    Add IllegalArgument Test

--- src/test/java/DoubleEndedList/DoubleLinkedListQueueTest.java
+++ src/test/java/DoubleEndedList/DoubleLinkedListQueueTest.java
@@ -9,13 +9,14 @@ public class DoubleLinkedListQueueTest {
     private DequeNode node1;
     private DequeNode node2;
     private DoubleLinkedListQueue list1;
+    private DoubleLinkedListQueue list2;
 
     @BeforeEach
-    public void init1() {
+    public void init() {
         node1 = new DequeNode<Integer>(1, null, null);
         node2 = new DequeNode<Integer>(1, null, null);
         list1 = new DoubleLinkedListQueue<Integer>();
-
+        list2 = new DoubleLinkedListQueue<>();
 
     }
 
@@ -26,8 +27,14 @@ public class DoubleLinkedListQueueTest {
         list1.append(node2);
 
         assertEquals(list1.peekLast(), node2);
-
     }
+
+    @Test
+    public void ShouldThrowAnExceptionIllegalArgumentWhenAppendNulls() {
+        assertThrows(IllegalArgumentException.class, () -> list1.append(null));
+        assertThrows(IllegalArgumentException.class, () -> list1.appendLeft(null));
+    }
+
 
     @Test
     public void ShouldAppendANodeFirstPosition() {
@@ -36,31 +43,33 @@ public class DoubleLinkedListQueueTest {
 
         assertEquals(list1.peekFirst(), node2);
     }
+
     @Test
-    public void ShouldDeleteNodeFirstPosition(){
+    public void ShouldDeleteNodeFirstPosition() {
         list1.append(node1);
         list1.append(node2);
 
         list1.deleteFirst();
 
-        assertEquals(list1.peekFirst(),node2);
+        assertEquals(list1.peekFirst(), node2);
     }
+
     @Test
-    public void ShouldDeleteNodeLastPosition(){
+    public void ShouldDeleteNodeLastPosition() {
         list1.append(node1);
         list1.append(node2);
 
         list1.deleteLast();
 
-        assertEquals(list1.peekLast(),node1);
+        assertEquals(list1.peekLast(), node1);
     }
 
     @Test
-    public void ShouldReturnTheSizeOfTheList(){
+    public void ShouldReturnTheSizeOfTheList() {
         list1.append(node1);
         list1.append(node2);
 
-        assertEquals(list1.size(),2);
+        assertEquals(list1.size(), 2);
     }
 
 
